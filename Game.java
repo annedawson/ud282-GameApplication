3064550801112ee7adde2763c352c1b614d1be36
annedawson <anne.dawson@gmail.com>
2018-01-21 13:40:43 -0800

    updated Game.java and Main.java Sun 21 Jan 13:40

--- Game.java
+++ Game.java
@@ -1,18 +1,22 @@
 package com.company;
 
-// updated: Fri Jan 19 9:50 PT
+// Updated: Sun 21 Jan 2018 13:33 PT
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+// String methods:
+// https://docs.oracle.com/javase/7/docs/api/java/lang/String.html
+
 public class Game {
     Scanner scanner = new Scanner(System.in);
 
     // Fields:
-    private String[] movieList;
+    private String[] movieList; // equivalent to private String movieList[];
     private int numberOfMovies;
     private String movie;
+    private String movieGuess;
     private File file = new File("movies.txt");
 
     // Constructor:
@@ -72,19 +76,25 @@ public class Game {
         return this.movie;
     }
 
+    public void setMovieGuessBlanks() {
+        this.movieGuess = this.movie.replaceAll(".", "_"); // "." is a regular expression meaning any character
+        System.out.println(this.movieGuess);
+    }
 
-
+    public void setMovieGuess(char theLetter) {
+        int n = 0;
+        while ( this.movie.indexOf(theLetter) != -1 ) {
+            this.movieGuess.replace(this.movie.charAt(this.movie.indexOf(theLetter)), theLetter);
+        }
         /*
-
-        System.out.println(numberOfMovies);
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello " + name + "!");
-        System.out.println("Enter your age: ");
-        int age = scanner.nextInt();
-        System.out.println("You are " + age + " years old");
-
+        HERE!! The above doesn't work ! infinite loop!
         */
+        System.out.println("In the setMovieGuess method");
+    }
+
+    public String getMovieGuess() {
+        return this.movieGuess;
+    }
 
 
 }
--- Main.java
+++ Main.java
@@ -1,13 +1,36 @@
 package com.company;
 
-// updated: Fri Jan 19 9:50 PT
+// Updated: Sun 21 Jan 2018 13:33 PT
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Game myGame = new Game();
+        Scanner scanner = new Scanner(System.in);
         myGame.setNumberOfMovies();
         myGame.setMovie();
+        System.out.println();
+        System.out.println();
+        System.out.println("Guess the movie!");
+        System.out.println("----------------");
+        System.out.println();
+        System.out.println();
         System.out.println(myGame.getMovie());
+        myGame.setMovieGuessBlanks();
+        System.out.println("Please enter a letter:");
+        char letter = scanner.next().charAt(0);
+        System.out.println(letter);
+        myGame.setMovieGuess(letter);
+        System.out.println(myGame.getMovieGuess());
+
+        /*
+
+        String name = scanner.nextLine();
+        int age = scanner.nextInt();
+        System.out.println("You are " + age + " years old");
+
+        */
     }
 }
